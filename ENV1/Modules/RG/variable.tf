42c761bbf0eac44e283c8d330537b3b82a15008b
Shahuls3 <vicksnews11@gmail.com>
2025-10-04 00:01:52 +0530

    M

--- ENV1/Modules/RG/variable.tf
+++ ENV1/Modules/RG/variable.tf
@@ -1,2 +1,2 @@
-Variable "resource_group_name" {}
+variable "resource_group_name" {}
 variable "location" {} 
--- ENV1/Prod1/main.tf
+++ ENV1/Prod1/main.tf
@@ -1,12 +1,12 @@
 module "rg" {
-  source              = "../../Modules/RG"
+  source              = "../Modules/RG"
   resource_group_name = "Prod-1"
   location            = "West Europe"
 }
 module "storage" {
-  source              = "../../Modules/Storage"
+  source              = "../Modules/Storage"
   depends_on          = [module.rg]
-  account_name        = "Prod-1stg"
+  account_name        = "Prod1stg"
   resource_group_name = "Prod-1"
   location            = "West Europe"
 }
