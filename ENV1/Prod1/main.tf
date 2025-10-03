5aef8d63f4f7d1a8cfbef84a1e7c8bb85d8153ae
Shahuls3 <vicksnews11@gmail.com>
2025-10-04 00:05:08 +0530

    d

--- ENV1/Prod1/main.tf
+++ ENV1/Prod1/main.tf
@@ -4,9 +4,9 @@ module "rg" {
   location            = "West Europe"
 }
 module "storage" {
-  source              = "../Modules/Storage"
-  depends_on          = [module.rg]
-  account_name        = "Prod1stg"
-  resource_group_name = "Prod-1"
-  location            = "West Europe"
+  source               = "../Modules/Storage"
+  depends_on           = [module.rg]
+  storage_account_name = "Prod1stg"
+  resource_group_name  = "Prod-1"
+  location             = "West Europe"
 }
